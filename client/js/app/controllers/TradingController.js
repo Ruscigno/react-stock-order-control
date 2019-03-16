21fe9eac05891063df3357067a69995c3c97fd08
Sander Ruscigno <ruscigno@gmail.com>
2019-03-16 20:51:40 -0300

    Improving performance

--- client/js/app/controllers/TradingController.js
+++ client/js/app/controllers/TradingController.js
@@ -1,11 +1,14 @@
 class TradingController{
 
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+    }
+
     adiciona(event){
         event.preventDefault();
 
-        let $ = document.querySelector.bind(document);
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
     }
 }
