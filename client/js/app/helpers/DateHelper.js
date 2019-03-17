d43f1750d0d10f86bcd6daf304fde94e450662df
Sander Ruscigno <ruscigno@gmail.com>
2019-03-16 22:01:27 -0300

    Using string template

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -4,12 +4,13 @@ class DateHelper{
         throw new Error("This class shouldn't be instanced");
     }
     static dataParaTexto(data){
-        data.getDate()
-        + '/' + (data.getMonth() + 1)
-        + '/' + data.getFullYear(); 
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
 
     static textoParaData(texto){
+        if (!/\d{4}-\d{2}-\d{2}/.test(texto)){
+            throw new Error('Should be in formart: yyyy-mm-dd');
+        }
         return new Date(texto.split('-'));
     }
 }
