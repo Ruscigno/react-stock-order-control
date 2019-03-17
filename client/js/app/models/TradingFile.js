be6887fd3609e9ca975a29012e99c07f33f4e550
Sander Ruscigno <ruscigno@gmail.com>
2019-03-16 22:00:33 -0300

    Add TradingFile class

---
+++ client/js/app/models/TradingFile.js
@@ -0,0 +1,14 @@
+class TradingFile{
+
+    constructor() {
+        this._negociacoes = []
+    }
+
+    adiciona(_negociacao){
+        this._negociacoes.push(_negociacao);
+    }
+
+    get negociacoes(){
+        return this.negociacoes;
+    }
+}
