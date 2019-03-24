d468389d74b56e5b1e4d1a3fff122817fa27059d
Sander Ruscigno <ruscigno@gmail.com>
2019-03-23 22:29:04 -0300

    decreasing verbosity

--- client/js/app/controllers/TradingController.js
+++ client/js/app/controllers/TradingController.js
@@ -6,19 +6,16 @@ class TradingController{
         this._inputquantity = $('#quantity');
         this._inputValor = $('#price');
 
-        this._tradingView = new TradingView($('#tradingViews'));
         this._tradingList = new Bind(
             new TradingList(),
-            this._tradingView,
-            ['add', 'clear']
+            new TradingView($('#tradingViews')),
+            'add', 'clear'
         );
-        
-        this._messageView = new MessageView($('#messageView'));
         
         this._message = new Bind(
             new Message(),
-            this._messageView,
-            ['text']
+            new MessageView($('#messageView')),
+            'text'
         );
     }
 
